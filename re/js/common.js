afe3b1aa81a6d21dd0b313187cf1ac2b96353e26
jeewon <40304444+jeewonbb@users.noreply.github.com>
2022-10-05 16:35:50 +0900

    Add files via upload
    js 업로드

--- re/js/common.js
+++ re/js/common.js
@@ -16,6 +16,32 @@ var animation1 = bodymovin.loadAnimation({
   autoplay: true // Optional
 });
 
+var animation1 = bodymovin.loadAnimation({
+  container: $('#Fly')[0], // Required
+  //path: 'https://assets2.lottiefiles.com/packages/lf20_muyl0kpg.json', // URL 직접 입력
+  path: 'js/fly.json', // 실제 사용 폴더 지정 ex) data.json
+  renderer: 'svg', // Required
+  loop: true, // Optional
+  autoplay: true // Optional
+});
+
+
+
+/*const playerContainers = document.querySelectorAll(".hoverEffects");
+playerContainers.forEach(container => {
+  container.addEventListener("mouseover", () => {
+    const player = container.querySelector("lottie-player");
+    player.setDirection(1);
+    player.play();
+  });
+
+  container.addEventListener("mouseleave", () => {
+    const player = container.querySelector("lottie-player");
+    player.setDirection(-1);
+    player.play();
+  });
+});*/
+
 $(document).ready(function () {
   $(".txt_list li").mouseover(function () {
       var tab_sel = $(this).attr("data-tab");
@@ -40,4 +66,35 @@ $(document).ready(function () {
     }, 500);
   }); 
 
+  $('.btn_top').on('click',function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 800);
+      return false;
+  });
+
+
+   var $wrap = $(".wrapper"),
+      pages = $(".page").length,
+      scrolling = false,
+      currentPage = 1,
+      $navPanel = $(".nav-panel"),
+      $scrollBtn = $(".scroll-btn"),
+    $navBtn = $(".nav-btn");
+  
+  
+  $(document).on("mousewheel DOMMouseScroll", function(e) {
+    if (!scrolling) {
+      if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+        navigateUp();
+      } else { 
+        navigateDown();
+      }
+    }
+  });
+
+
+
+
 });
+
